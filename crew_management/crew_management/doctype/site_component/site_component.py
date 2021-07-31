3e2bf428eaa7373180b524268fbde2cbc3bd555e
Bao Ho <hotribao@gmail.com>
2021-07-31 16:16:41 +0700

    filter can be none

--- crew_management/crew_management/doctype/site_component/site_component.py
+++ crew_management/crew_management/doctype/site_component/site_component.py
@@ -22,22 +22,29 @@ class SiteComponent(Document):
 def site_component_query(doctype, txt, searchfield, start, page_len, filters):
     from frappe.desk.reportview import get_match_cond
 
-    site_uid = filters.get('site')
-    if site_uid is None or site_uid == '':
-        return []
+    # filters is None when calling from the assignment's Job
+
+    site_uid = filters.get('site') if filters is not None else ''
+    component_of_same_site_condition = ''
+    if site_uid is not None and site_uid != '':
+        component_of_same_site_condition = 'AND parent = %(site_uid)s'
 
     exclude_current_component_condition = ''
-    current_component_uid = filters.get('self_uid')
+    current_component_uid = filters.get('self_uid') if filters is not None else ''
     if current_component_uid is not None and current_component_uid != '':
         exclude_current_component_condition = 'AND name <> %(current_component_uid)s'
+
+    component_name_condition = ''
+    if txt is not None and txt != '':
+        component_name_condition = 'AND component_name LIKE %(txt)s'
 
     return frappe.db.sql("""
         SELECT name, label, component_name
         FROM `tabSite Component`
         WHERE
             docstatus < 2
-            AND component_name LIKE %(txt)s
-            AND parent = %(site_uid)s
+            {component_name_condition}
+            {component_of_same_site_condition}
             {exclude_current_component_condition}
             {mcond}
         ORDER BY
@@ -45,7 +52,9 @@ def site_component_query(doctype, txt, searchfield, start, page_len, filters):
         LIMIT %(start)s, %(page_len)s
     """.format(**{
         'mcond': get_match_cond(doctype),
-        'exclude_current_component_condition': exclude_current_component_condition
+        'exclude_current_component_condition': exclude_current_component_condition,
+        'component_of_same_site_condition': component_of_same_site_condition,
+        'component_name_condition': component_name_condition
     }), {
          'txt': "%{}%".format(txt),
          'site_uid': site_uid,
