ee2a272f1cc33de5a8b4f855cd9559ea3072d21b
PhucDoan <doannicolas@gmail.com>
2021-08-23 08:27:04 +0700

    PD#1-filter team member by type

--- crew_management/crew_management/doctype/team/team.js
+++ crew_management/crew_management/doctype/team/team.js
@@ -1,9 +1,16 @@
-
 // Copyright (c) 2021, Zirrus One and contributors
 // For license information, please see license.txt
 
 frappe.ui.form.on('Team', {
-	// refresh: function(frm) {
-
-	// }
+    setup: function (frm) {
+        // frm.trigger('set_team_member_query')
+        frm.set_query('team_member', function () {
+            return {
+                query: "crew_management.crew_management.doctype.team.team.team_member_query",
+                filters: {
+                    'team_type': frm.doc.team_type
+                }
+            }
+        })
+    },
 });
--- crew_management/crew_management/doctype/team/team.py
+++ crew_management/crew_management/doctype/team/team.py
@@ -1,8 +1,72 @@
 # Copyright (c) 2021, Zirrus One and contributors
 # For license information, please see license.txt
 
-# import frappe
+from __future__ import unicode_literals
+
+import frappe
 from frappe.model.document import Document
+from crew_management.crew_management.doctype.team_member.team_member import get_member_based_on_role
+
+STANDARD_USERS = ("Guest", "Administrator")
+CREW_BACK_OFFICE_ROLES = ("BackOfficeStaff")
+CREW_FIELD_ROLES = ("Field Lead", "Field Installer")
 
 class Team(Document):
-	pass
+	def before_insert(self):
+		if frappe.db.exists("Team", {"name": self.name}):
+			frappe.throw(frappe._("Team '{0}' already existed").format(self.name))
+
+
+@frappe.whitelist()
+@frappe.validate_and_sanitize_search_inputs
+def team_member_query(doctype, txt, searchfield, start, page_len, filters):
+	from frappe.desk.reportview import get_match_cond, get_filters_cond
+	conditions = []
+
+	user_type_condition = "and user_type != 'Website User'"
+	if filters and filters.get('ignore_user_type'):
+		user_type_condition = ''
+		filters.pop('ignore_user_type')
+
+	if filters and filters.get('team_type'):
+		team_type = filters.get('team_type')
+		if team_type == "Back Office":
+			team_roles = ", ".join([frappe.db.escape("Back Office Staff")])
+		elif team_type == "Field Crew":
+			team_roles = ", ".join([frappe.db.escape(u) for u in CREW_FIELD_ROLES])
+		else:
+			team_roles = ''
+		filters.pop('team_type')
+
+	txt = "%{}%".format(txt)
+	return frappe.db.sql("""
+		SELECT 
+			usr.name, 
+			CONCAT_WS(' ', usr.first_name, usr.middle_name, usr.last_name)
+		FROM `tabUser` usr
+		INNER JOIN `tabHas Role` usr_role
+			ON (usr.name = usr_role.parent) 
+		WHERE `enabled`=1
+			{user_type_condition}
+			AND usr_role.role IN ({team_roles})
+			AND usr.docstatus < 2
+			AND usr.name NOT IN ({standard_users})
+			AND ({key} LIKE %(txt)s
+				OR CONCAT_WS(' ', usr.first_name, usr.middle_name, usr.last_name) LIKE %(txt)s)
+			{fcond} {mcond}
+		ORDER BY
+			CASE WHEN usr.name LIKE %(txt)s THEN 0 ELSE 1 END,
+			CASE WHEN CONCAT_WS(' ', usr.first_name, usr.middle_name, usr.last_name) LIKE %(txt)s
+				THEN 0 ELSE 1 END,
+			NAME asc
+		LIMIT %(page_len)s OFFSET %(start)s
+	""".format(
+		user_type_condition=user_type_condition,
+		team_roles=team_roles,
+		standard_users=", ".join([frappe.db.escape(u) for u in STANDARD_USERS]),
+		key='usr.' + searchfield,
+		fcond=get_filters_cond(doctype, filters, conditions),
+		mcond=get_match_cond(doctype)
+	),
+		dict(start=start, page_len=page_len, txt=txt)
+	)
