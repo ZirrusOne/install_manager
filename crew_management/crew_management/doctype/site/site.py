fbf4dbe8e4a569ea8064029e91f37b4d1e7919d6
PhucDoan <doannicolas@gmail.com>
2021-08-31 18:28:36 +0700

    PD#2-Site validate site contact

--- crew_management/crew_management/doctype/site/site.py
+++ crew_management/crew_management/doctype/site/site.py
@@ -4,11 +4,13 @@
 import frappe
 from frappe import _
 from frappe.model.document import Document
+from frappe.utils import validate_email_address, validate_phone_number
 from crew_management.crew_management.controllers.states import POSTAL_CODES
 
 class Site(Document):
 	def validate(self):
 		self.validate_zip_code()
+		self.validate_site_contact()
 
 	def validate_zip_code(self):
 		zip_codes = [code for code in POSTAL_CODES if code[0] == self.state and code[1].lower() == self.city.strip().lower()]
@@ -16,12 +18,29 @@ class Site(Document):
 			if self.zip_code >= code[2] and self.zip_code <= code[3]:
 				return
 
+		frappe.throw(_("Zip code invalid. ({0})").format(self.zip_code))
 
-		frappe.msgprint(_("Zip code invalid. ({0})").format(self.zip_code),
-						title='Error',
-						indicator='red',
-						raise_exception=1)
+	def validate_site_contact(self):
+		check_email_list = []
+		for contact in self.get('site_contact'):
+			if contact.first_name:
+				contact.first_name = contact.first_name.capitalize()
 
+			if contact.last_name:
+				contact.last_name = contact.last_name.capitalize()
+
+			if contact.email_address:
+				if contact.email_address not in check_email_list:
+					validate_email_address(contact.email_address, True)
+					check_email_list.append(contact.email_address)
+				else:
+					frappe.throw(_("{0} entered twice in Site Contact").format(contact.email_address))
+
+			if contact.office_phone:
+				validate_phone_number(contact.office_phone, True)
+
+			if contact.mobile_phone:
+				validate_phone_number(contact.mobile_phone, True)
 
 @frappe.whitelist()
 def get_state_options():
