8f1ccdeebdd6f43c2b68b8916ca8dc9b18dd17b9
cangle <cangle.tn@gmail.com>
2021-08-31 15:04:48 +0700

    get job base user

--- crew_management/crew_management/page/job_management/job_management.js
+++ crew_management/crew_management/page/job_management/job_management.js
@@ -4,12 +4,44 @@
 ///
 
 frappe.pages['job-management'].on_page_load = function (wrapper) {
-    var page = frappe.ui.make_app_page({
+    let aThis = this;
+    let page = frappe.ui.make_app_page({
         parent: wrapper,
         title: 'Job Management',
         single_column: true
     });
 
     page.main.html(frappe.render_template('job_management', {}));
+    get_data(aThis);
+}
 
+let get_data = function (aThis) {
+    let content = "";
+    frappe.call({
+        'method': 'crew_management.crew_management.page.job_management.job_management.get_job_base_team',
+        callback: function (r) {
+            let data = JSON.parse(r.message);
+            if (data.length) {
+                console.log(data)
+                let i;
+                for (i = 0; i < data.length; i++) {
+                    let teamWrapper = "<div class='team-wrapper'><div class='team-title'>" + data[i].team + "</div>"
+                    const listJob = data[i].jobs;
+                    if (listJob) {
+                        let j;
+                        teamWrapper += "<div class='list-job'>"
+                        for (j = 0; j < listJob.length; j++) {
+                            let jobItem = "<div class='job-item'>" + listJob[j].label + " " + listJob[j].component_name + " - " + listJob[j].status + "</div>"
+                            teamWrapper += jobItem;
+                        }
+                        teamWrapper += "</div></div>"
+                    }
+                    content += teamWrapper;
+                }
+                aThis.page.main.find('.job-wrapper').append(content);
+            } else {
+                aThis.page.main.find('.job-wrapper').append('No result');
+            }
+        }
+    });
 }
--- crew_management/crew_management/page/job_management/job_management.py
+++ crew_management/crew_management/page/job_management/job_management.py
@@ -2,3 +2,27 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
+import frappe
+from itertools import groupby
+
+
+@frappe.whitelist()
+def get_job_base_team():
+    current_user = frappe.get_doc('User', frappe.session.user)
+    results = [];
+    query = frappe.db.sql("""
+        SELECT job.assigned_team , siteC.label, siteC.component_name, job.idx, job.status , job.parent
+        FROM `tabJob` job
+		INNER JOIN `tabSite Component` siteC ON (job.site_component = siteC.name) 
+		WHERE job.assigned_team in (Select distinct parent 
+                            from `tabTeam Member`
+                            where member = %(member)s)
+		ORDER BY job.assigned_team
+    """, dict(member=current_user.email), as_dict=True)
+
+    groups = groupby(query, lambda content: content['assigned_team'])
+    for key, group in groups:
+        item = {'team': key, 'jobs': list(group)}
+        results.append(item)
+
+    return frappe.as_json(results)
