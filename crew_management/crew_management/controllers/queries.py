42cbf2effe633b2d7524e37af54d972e5a0b2d11
Bao Ho <hotribao@gmail.com>
2021-09-15 18:34:26 +0700

    fix error: pymysql.err.OperationalError: (1054, "Unknown column 'tabUser.name' in 'where clause'")

--- crew_management/crew_management/controllers/queries.py
+++ crew_management/crew_management/controllers/queries.py
@@ -34,22 +34,22 @@ def team_member_query(doctype, txt, searchfield, start, page_len, filters):
     txt = "%{}%".format(txt)
     return frappe.db.sql("""
 		SELECT 
-			usr.name, 
-			CONCAT_WS(' ', usr.first_name, usr.middle_name, usr.last_name)
-		FROM `tabUser` usr
+			tabUser.name, 
+			CONCAT_WS(' ', tabUser.first_name, tabUser.middle_name, tabUser.last_name)
+		FROM tabUser
 		INNER JOIN `tabHas Role` usr_role
-			ON (usr.name = usr_role.parent) 
+			ON (tabUser.name = usr_role.parent) 
 		WHERE `enabled`=1
 			{user_type_condition}
 			AND usr_role.role IN ({team_roles})
-			AND usr.docstatus < 2
-			AND usr.name NOT IN ({standard_users})
+			AND tabUser.docstatus < 2
+			AND tabUser.name NOT IN ({standard_users})
 			AND ({key} LIKE %(txt)s
-				OR CONCAT_WS(' ', usr.first_name, usr.middle_name, usr.last_name) LIKE %(txt)s)
+				OR CONCAT_WS(' ', tabUser.first_name, tabUser.middle_name, tabUser.last_name) LIKE %(txt)s)
 			{fcond} {mcond}
 		ORDER BY
-			CASE WHEN usr.name LIKE %(txt)s THEN 0 ELSE 1 END,
-			CASE WHEN CONCAT_WS(' ', usr.first_name, usr.middle_name, usr.last_name) LIKE %(txt)s
+			CASE WHEN tabUser.name LIKE %(txt)s THEN 0 ELSE 1 END,
+			CASE WHEN CONCAT_WS(' ', tabUser.first_name, tabUser.middle_name, tabUser.last_name) LIKE %(txt)s
 				THEN 0 ELSE 1 END,
 			NAME asc
 		LIMIT %(page_len)s OFFSET %(start)s
@@ -57,7 +57,7 @@ def team_member_query(doctype, txt, searchfield, start, page_len, filters):
         user_type_condition=user_type_condition,
         team_roles=team_roles,
         standard_users=", ".join([frappe.db.escape(u) for u in STANDARD_USERS]),
-        key='usr.' + searchfield,
+        key='tabUser.' + searchfield,
         fcond=get_filters_cond(doctype, filters, conditions),
         mcond=get_match_cond(doctype)
     ),
