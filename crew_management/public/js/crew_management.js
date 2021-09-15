0f06e3a098f0f5a2094b09d8bff31e0e9b90d644
cangle <cangle.tn@gmail.com>
2021-09-15 09:20:00 +0700

    remove unused code

--- crew_management/public/js/crew_management.js
+++
@@ -1,9 +0,0 @@
-$(document).on("startup", function () {
-    if (frappe.user.has_role("System Manager")) {
-        frappe.set_route("/home");
-    } else if (frappe.user.has_role("Back Office Staff")) {
-        frappe.set_route("/crew-management");
-    } else if (frappe.user.has_role("Field Installer") || frappe.user.has_role("Field Lead")) {
-        frappe.set_route("/assignment")
-    }
-});
