e221f7c838ec5bb8172642354886ccae2e57aaa9
Bao Ho <hotribao@gmail.com>
2021-09-20 10:39:15 +0700

    #3 make sure Assignment's parent company is set to the default company

--- crew_management/crew_management/doctype/assignment/assignment.py
+++ crew_management/crew_management/doctype/assignment/assignment.py
@@ -2,7 +2,20 @@
 # For license information, please see license.txt
 
 # import frappe
+import frappe
 from frappe.model.document import Document
 
+
 class Assignment(Document):
-	pass
+
+	def db_update(self):
+		self._fill_default_company()
+		super(Assignment, self).db_update()
+
+	def db_insert(self):
+		self._fill_default_company()
+		super(Assignment, self).db_insert()
+
+	def _fill_default_company(self):
+		if self.parent_company is None or self.parent_company == '':
+			self.parent_company = frappe.db.get_value('Global Defaults', None, 'default_company')
