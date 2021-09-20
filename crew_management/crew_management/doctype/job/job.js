c8708a810d0218a74c486d00521699ba1747aef9
cangle <cangle.tn@gmail.com>
2021-09-20 17:09:18 +0700

    #15: update layout job detail

--- crew_management/crew_management/doctype/job/job.js
+++ crew_management/crew_management/doctype/job/job.js
@@ -2,7 +2,17 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Job', {
-	// refresh: function(frm) {
-
-	// }
+    onload: function (frm) {
+        if (frappe.user.has_role("System Manager") || frappe.user.has_role("Administrator")) {
+            $('#navbar-breadcrumbs').removeClass('hide-item');
+            $('.sidebar-toggle-btn').removeClass('hide-item');
+            $('.layout-side-section').removeClass('hide-item');
+            $('.menu-btn-group').removeClass('hide-item');
+        } else if (frappe.user.has_role("Field Lead") || frappe.user.has_role("Field Lead")) {
+            $('#navbar-breadcrumbs').addClass('hide-item');
+            $('.sidebar-toggle-btn').addClass('hide-item');
+            $('.layout-side-section').addClass('hide-item');
+            $('.menu-btn-group').addClass('hide-item');
+        }
+    }
 });
