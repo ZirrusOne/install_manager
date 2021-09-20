0dc2a383188e7541bf95a2483dcbf10018f78e70
Bao Ho <hotribao@gmail.com>
2021-09-20 22:05:52 +0700

    update the denormalized field assignment_name in Job

--- crew_management/crew_management/doctype/assignment/assignment.py
+++ crew_management/crew_management/doctype/assignment/assignment.py
@@ -8,14 +8,25 @@ from frappe.model.document import Document
 
 class Assignment(Document):
 
-	def db_update(self):
-		self._fill_default_company()
-		super(Assignment, self).db_update()
-
 	def db_insert(self):
 		self._fill_default_company()
 		super(Assignment, self).db_insert()
 
+	def db_update(self):
+		self._fill_default_company()
+		is_assignment_name_change = self.assignment_name != self.get_db_value('assignment_name')
+		super(Assignment, self).db_update()
+		if is_assignment_name_change:
+			self._update_linked_assignment_names()
+
 	def _fill_default_company(self):
 		if self.parent_company is None or self.parent_company == '':
 			self.parent_company = frappe.db.get_value('Global Defaults', None, 'default_company')
+
+	def _update_linked_assignment_names(self):
+		conditions, values = frappe.db.build_conditions({'assignment': ('=', self.name)})
+		values['assignment_name'] = self.assignment_name
+
+		frappe.db.sql("UPDATE `tabJob` SET assignment_name =  %(assignment_name)s WHERE {conditions}".format(
+			conditions=conditions
+		), values, debug=False)
