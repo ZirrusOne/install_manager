9f1a0c4b53d205d83421b43de91b4b6ea4794b72
cangle <cangle.tn@gmail.com>
2021-09-20 17:11:09 +0700

    #15: update layout job detail

--- crew_management/crew_management/doctype/job/job.js
+++ crew_management/crew_management/doctype/job/job.js
@@ -8,7 +8,7 @@ frappe.ui.form.on('Job', {
             $('.sidebar-toggle-btn').removeClass('hide-item');
             $('.layout-side-section').removeClass('hide-item');
             $('.menu-btn-group').removeClass('hide-item');
-        } else if (frappe.user.has_role("Field Lead") || frappe.user.has_role("Field Lead")) {
+        } else if (frappe.user.has_role("Field Lead") || frappe.user.has_role("Field Installer")) {
             $('#navbar-breadcrumbs').addClass('hide-item');
             $('.sidebar-toggle-btn').addClass('hide-item');
             $('.layout-side-section').addClass('hide-item');
