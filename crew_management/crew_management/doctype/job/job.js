453cef19d0f0c0b6de24308d2a404f0a756f94c7
cangle <cangle.tn@gmail.com>
2021-09-20 23:59:46 +0700

    update condition for multi role

--- crew_management/crew_management/doctype/job/job.js
+++ crew_management/crew_management/doctype/job/job.js
@@ -14,12 +14,10 @@ frappe.ui.form.on('Job', {
     },
 
     onload: function (frm) {
-        if (frappe.user.has_role("System Manager") || frappe.user.has_role("Administrator")) {
-            $('#navbar-breadcrumbs').removeClass('hide-item');
-            $('.sidebar-toggle-btn').removeClass('hide-item');
-            $('.layout-side-section').removeClass('hide-item');
-            $('.menu-btn-group').removeClass('hide-item');
-        } else if (frappe.user.has_role("Field Lead") || frappe.user.has_role("Field Installer")) {
+        let is_field_crew = (frappe.user.has_role("Field Lead") || frappe.user.has_role("Field Installer")) && !frappe.user.has_role("Back Office Staff");
+        let is_back_office_staff = frappe.user.has_role("Back Office Staff");
+
+        if (is_field_crew) {
             $('#navbar-breadcrumbs').addClass('hide-item');
             $('.sidebar-toggle-btn').addClass('hide-item');
             $('.layout-side-section').addClass('hide-item');
@@ -28,6 +26,11 @@ frappe.ui.form.on('Job', {
                 let statusField = frm.fields.find(item => item.df.fieldname === 'status')
                 statusField.df.options = ["Pending", "In Progress", "Non-compliant", "Completed"];
             }
+        } else {
+            $('#navbar-breadcrumbs').removeClass('hide-item');
+            $('.sidebar-toggle-btn').removeClass('hide-item');
+            $('.layout-side-section').removeClass('hide-item');
+            $('.menu-btn-group').removeClass('hide-item');
         }
     },
 
