56691ceab6a0beb3fa8b3f7459fcd968afffa542
cangle <cangle.tn@gmail.com>
2021-09-21 18:42:54 +0700

    #19 add white list url check role field lead/installer

--- crew_management/public/js/handle_route_change.js
+++ crew_management/public/js/handle_route_change.js
@@ -1,7 +1,13 @@
 let url = location.href;
+
+$(document).ready(function () {
+    checkFieldCrewWhiteList();
+});
+
 document.body.addEventListener('click', () => {
     requestAnimationFrame(() => {
         let currentRoute = location.href;
+        checkFieldCrewWhiteList();
         if (url !== currentRoute) {
             url = location.href;
             if (!currentRoute.includes('job-management')) {
@@ -15,3 +21,24 @@ document.body.addEventListener('click', () => {
         }
     });
 }, true);
+
+function checkFieldCrewWhiteList() {
+    const field_crew_url_white_list = [
+        "app/user-profile",
+        "app/job-management",
+        "app/user",
+        "app/job/JOB-",
+    ]
+    if ((frappe.user.has_role("Field Lead") || frappe.user.has_role("Field Installer")) && !frappe.user.has_role("Back Office Staff")) {
+        let inWhiteListUrl = false;
+        field_crew_url_white_list.forEach(item => {
+            if (location.href.includes(item)) {
+                inWhiteListUrl = true;
+            }
+        })
+
+        if (!inWhiteListUrl) {
+            window.location.href = "/app/job-management"
+        }
+    }
+}
