339d349de20d953fcfe6b2ee41da3b60435684b7
Bao Ho <hotribao@gmail.com>
2021-09-21 11:30:35 +0700

    validate assignment team type

--- crew_management/crew_management/doctype/assignment/assignment.py
+++ crew_management/crew_management/doctype/assignment/assignment.py
@@ -10,11 +10,14 @@ class Assignment(Document):
 
 	def db_insert(self):
 		self._fill_default_company()
+		self._validate_team_type()
 		super(Assignment, self).db_insert()
 
 	def db_update(self):
 		self._fill_default_company()
+		self._validate_team_type()
 		is_assignment_name_change = self.assignment_name != self.get_db_value('assignment_name')
+
 		super(Assignment, self).db_update()
 		if is_assignment_name_change:
 			self._update_linked_assignment_names()
@@ -30,3 +33,11 @@ class Assignment(Document):
 		frappe.db.sql("UPDATE `tabJob` SET assignment_name =  %(assignment_name)s WHERE {conditions}".format(
 			conditions=conditions
 		), values, debug=False)
+
+	def _validate_team_type(self):
+		# back_office_team is a list of AssignmentBackOffice object
+		if self.back_office_team is not None and len(self.back_office_team) > 0:
+			for assignmentBo in self.back_office_team:
+				team_type = frappe.db.get_value('Team', {'name': assignmentBo.team}, 'team_type')
+				if team_type != 'Back Office':
+					frappe.throw(f'Team type of team {assignmentBo.team} is not Back Office')
