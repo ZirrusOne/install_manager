98a65c4de77b413e8fc9541e01c0f60ef278f57a
Bao Ho <hotribao@gmail.com>
2021-11-10 14:49:25 +0700

    avoid displaying 2 columns with title "Name"

--- install_manager/install_manager/doctype/team/team_list.js
+++ install_manager/install_manager/doctype/team/team_list.js
@@ -1,7 +1,4 @@
 frappe.listview_settings['Team'] = {
-    hide_name_column: true,
     onload: function () {
-        $('.page-form .standard-filter-section div[data-fieldname="name"]').remove();
-        $('.page-form .filter-section .sort-selector .sort-selector-button ul li a[data-value="name"]').remove();
     }
 }
