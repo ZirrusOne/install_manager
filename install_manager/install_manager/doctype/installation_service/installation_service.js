ed4a2d705730840decc3dee15fdc3d43bacfda32
Bao Ho <hotribao@gmail.com>
2021-11-11 14:09:10 +0700

    add multi-selection field "additional services" to JOb

---
+++ install_manager/install_manager/doctype/installation_service/installation_service.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Zirrus One and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Installation Service', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ install_manager/install_manager/doctype/installation_service/test_installation_service.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, Zirrus One and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestInstallationService(unittest.TestCase):
+	pass
