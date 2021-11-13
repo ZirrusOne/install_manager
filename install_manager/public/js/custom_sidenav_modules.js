857ca5fc7120a4e025bed6e7d4e490000956d838
Bao Ho <hotribao@gmail.com>
2021-11-13 11:24:32 +0700

    rework desk's sidebar

--- install_manager/public/js/custom_sidenav_modules.js
+++ install_manager/public/js/custom_sidenav_modules.js
@@ -1,19 +1,67 @@
 $(document).ready(function () {
-    let is_back_office = frappe.user.has_role("Back Office Staff") && !frappe.user.has_role("Administrator");
-    let module_node = $('.standard-sidebar-label:contains("Modules")');
+    let isBackOffice = frappe.user.has_role("Back Office Staff") && !frappe.user.has_role("Administrator");
+    let moduleNodeElm = $('.standard-sidebar-label:contains("Modules")');
+    let sideBarModuleSectionElm = moduleNodeElm.parent();
 
-    if (is_back_office && module_node) {
-        module_node.parent().each(function(){
-            let remove_items = [];
-            this.childNodes.forEach(item => {
-                if (item.classList.contains('desk-sidebar-item') && item.innerText !== 'Install Manager') {
-                    remove_items.push(item);
-                }
-            })
+    if (isBackOffice) {
+        removeChildren(sideBarModuleSectionElm, 'Install Manager', 'DASHBOARD');
 
-            remove_items.forEach(item => {
-                this.removeChild(item);
-            })
-        });
+        moduleNodeElm.text(frappe.defaults.get_user_default("Company"));
+        moduleNodeElm.css('text-transform', 'none');
+
+        let installManagerElm = sideBarModuleSectionElm.children('a[href^="/app/install-manager"]');
+        installManagerElm
+            .after([
+                getSideBarItemHtml('TEAMS', '/app/team', 'users'),
+                getSideBarItemHtml('SCHEDULES', '/app/schedule', 'calendar'),
+                getSideBarItemHtml('SITES', '/app/site', 'organization')
+            ].join(''));
+        installManagerElm.html(getSideBarItemInner('DASHBOARD', 'crm'));  // icon dashboard
+
+        let administrationElm = $('.standard-sidebar-label:contains("Administration")');
+        removeChildren(administrationElm.parent(), 'Users');
+        administrationElm.parent().children('a[href^="/app/users"]').children('.sidebar-item-label').html('USERS');
+    } else {
+        let installManagerElm = sideBarModuleSectionElm.children('a[href^="/app/install-manager"]');
+        installManagerElm
+            .after([
+                getSideBarItemHtml('Teams', '/app/team', 'users'),
+                getSideBarItemHtml('Schedules', '/app/schedule', 'calendar'),
+                getSideBarItemHtml('Sites', '/app/site', 'organization')
+            ].join(''));
+        installManagerElm.html(getSideBarItemInner('Install Manager', 'projects'));
     }
+
 })
+
+function removeChildren(containerElm, keepItemWithText) {
+    containerElm.each(function() {
+        let removeItems = [];
+        this.childNodes.forEach(item => {
+            if (item.classList.contains('desk-sidebar-item') && item.innerText.toLowerCase() !== keepItemWithText.toLowerCase()) {
+                removeItems.push(item);
+            }
+        })
+
+        removeItems.forEach(item => {
+            this.removeChild(item);
+        })
+    });
+}
+
+function getSideBarItemHtml(name, uri, icon) {
+    return [
+        '<a href="', uri, '" class="desk-sidebar-item standard-sidebar-item">',
+        getSideBarItemInner(name, icon),
+        '</a>'
+    ].join('');
+}
+
+function getSideBarItemInner(name, icon) {
+    return [
+        '   <span>',
+        frappe.utils.icon(icon || "folder-normal", "md"),
+        '   </span>',
+        '   <span class="sidebar-item-label">', name, '</span>',
+    ].join('');
+}
