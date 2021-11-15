daca726f8ac3ed20d8a4cf88266afe4f8952ab78
Bao Ho <hotribao@gmail.com>
2021-11-15 14:14:31 +0700

    customize default dark theme

--- install_manager/public/js/custom_theme.js
+++ install_manager/public/js/custom_theme.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    $('html').addClass('z1n');
+
     // in Frappe, theme is personalized to individual users. Them "light" is default if the user
     // has not chosen any other theme for himself/herself. Frappe doesn't provide any means (as of version 13)
     // to change the default theme (see file app.py in frappe framework). What we did here is kind of hacking.
