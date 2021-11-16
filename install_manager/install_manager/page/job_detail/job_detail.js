3de9ca1b2440126e80339a0ab43ef66dd016a901
cangle <cangle.tn@gmail.com>
2021-11-16 23:13:27 +0700

    implement header, job type, additional service.

--- install_manager/install_manager/page/job_detail/job_detail.js
+++ install_manager/install_manager/page/job_detail/job_detail.js
@@ -17,12 +17,24 @@ class JobDetail {
     statusEscalateElement;
     addCommentElement;
     addPhotoElement;
+    jobDetailElement;
 
     job_id;
     job_detail;
-    job_types = [];
+    schedule;
+    site;
+    site_unit;
 
-    selectedJobType;
+    job_statuses = [];
+    escalations_reasons = [];
+    non_compliant_reasons = [];
+    installation_types = [];
+    additional_services = [];
+
+    selectedInstallationType;
+    selectedJobStatus;
+
+    isAdditionalServiceChanged = false;
 
     constructor(wrapper) {
     }
@@ -33,17 +45,15 @@ class JobDetail {
         if (this.job_id === '' || !this.job_id.startsWith('JOB-')) {
             window.location.href = '/app/job-management';
         }
-        console.log(this.job_id)
         $(wrapper).empty();
+
         this.page = frappe.ui.make_app_page({
             parent: wrapper,
             title: 'Job Details',
             single_column: true
         });
 
-        $(frappe.render_template('job_detail', {
-            isInstaller: frappe.user.has_role("Field Installer")
-        })).appendTo($(this.page.main));
+        $(frappe.render_template('job_detail', {})).appendTo($(this.page.main));
 
         this.jobTypeElement = $(this.page.main).find('#jobType');
         this.additionalServiceElement = $(this.page.main).find('#additionalService');
@@ -52,7 +62,28 @@ class JobDetail {
         this.statusEscalateElement = $(this.page.main).find('#statusEscalate');
         this.addCommentElement = $(this.page.main).find('#addComment');
         this.addPhotoElement = $(this.page.main).find('#addPhoto');
-
+        this.jobDetailElement = $(this.page.main).find('.job-detail-wrapper');
+        const aThis = this;
+        //get additional services
+        frappe.call({
+            method: 'frappe.desk.reportview.get',
+            args: {
+                doctype: "Installation Service",
+                fields: ["name"]
+            },
+            type: 'GET',
+            callback: function (result) {
+                aThis.additional_services = [];
+                let additional_services = result.message.values
+                additional_services.forEach(item => {
+                    let additional_service = {
+                        name: item[0],
+                        isSelected: false
+                    }
+                    aThis.additional_services.push(additional_service);
+                })
+            }
+        });
         this.getData();
         this.customUI();
     }
@@ -60,19 +91,24 @@ class JobDetail {
     openJobTypeModal() {
         this.renderJobType();
         $('#jobTypeModal').modal('show');
-
     }
 
     renderJobType() {
         $(this.jobTypeElement).empty();
         $(frappe.render_template('job_type', {
-            job_types: this.job_types,
-            selectedJobType: this.selectedJobType
+            installation_types: this.installation_types,
+            selectedInstallationType: this.selectedInstallationType
         })).appendTo($(this.jobTypeElement));
     }
 
     onChangeJobType(element) {
-        $('#jobTypeModal').modal('hide');
+        let value = $(element).attr('data-value');
+        if (value !== this.selectedInstallationType) {
+            this.selectedInstallationType = value;
+            this.saveJob();
+            $('#jobTypeModal').modal('hide');
+            this.getData();
+        }
     }
 
     openAdditionalServiceModal() {
@@ -82,14 +118,31 @@ class JobDetail {
 
     renderAdditionalService() {
         $(this.additionalServiceElement).empty();
-        $(frappe.render_template('additional_service', {})).appendTo($(this.additionalServiceElement));
+        $(frappe.render_template('additional_service', {
+            additional_services: this.additional_services
+        })).appendTo($(this.additionalServiceElement));
     }
 
-    onChangeAdditionalService() {
+    onChangeAdditionalService(element) {
+        let service = $(element).attr('data-value');
+        this.isAdditionalServiceChanged = true;
+        let foundService = this.additional_services.find(item => item.name === service);
+        if (foundService) {
+            foundService.isSelected = !foundService.isSelected;
+            if (foundService.isSelected) {
+                $(element).addClass('active');
+            } else {
+                $(element).removeClass('active');
+            }
+        }
 
     }
 
     onCloseAdditionalServiceModal() {
+        if (this.isAdditionalServiceChanged) {
+            this.saveJob();
+            this.getData();
+        }
         $('#additionalServiceModal').modal('hide');
     }
 
@@ -103,7 +156,7 @@ class JobDetail {
         $(frappe.render_template('job_status', {})).appendTo($(this.jobStatusElement));
     }
 
-    onChangeJobType(element) {
+    onChangeJobStatus(element) {
         $('#jobStatusModal').modal('hide');
     }
 
@@ -116,11 +169,40 @@ class JobDetail {
     }
 
     getData() {
-        frappe.model
-            .with_doc("Job", this.job_id)
+
+        frappe.model.with_doctype("Job", () => {
+            let meta = frappe.get_meta("Job");
+            this.job_statuses = meta.fields.find(item => item.fieldname === "status").options.split("\n");
+            this.escalations_reasons = meta.fields.find(item => item.fieldname === "escalation_reason").options.split("\n");
+            this.non_compliant_reasons = meta.fields.find(item => item.fieldname === "non_compliant_reason").options.split("\n");
+            this.installation_types = meta.fields.find(item => item.fieldname === "installation_type").options.split("\n");
+        });
+
+        let result = frappe.model.with_doc("Job", this.job_id)
             .then((result) => {
                 this.job_detail = result
-            });
+                this.selectedInstallationType = this.job_detail.installation_type;
+
+                if (this.job_detail.additional_services.length > 0) {
+                    this.job_detail.additional_services.forEach(item => {
+                        let foundItem = this.additional_services.find(service => service.name === item.service);
+                        if (foundItem)
+                            foundItem.isSelected = true;
+                    })
+                }
+
+                let siteUnit = frappe.model.with_doc("Site Unit", this.job_detail.site_unit)
+                let schedule = frappe.model.with_doc("Schedule", this.job_detail.schedule)
+                Promise.all([siteUnit, schedule]).then((result) => {
+                    this.site_unit = result[0];
+                    this.schedule = result[1];
+                    let site = frappe.model.with_doc("Site", this.schedule.site)
+                    Promise.all([site]).then((siteResult) => {
+                        this.site = siteResult[0];
+                        this.renderJobDetail()
+                    })
+                })
+            })
     }
 
     clearData() {
@@ -132,7 +214,7 @@ class JobDetail {
         frappe.call({
             freeze: true,
             method: "frappe.desk.form.save.savedocs",
-            args: {doc: this.jobDetail, action: "Save"},
+            args: {doc: this.job_detail, action: "Save"},
             callback: function (result) {
 
             }
@@ -140,8 +222,33 @@ class JobDetail {
     }
 
     prepareDataBeforeSave() {
+        this.job_detail.installation_type = this.selectedInstallationType;
+        this.job_detail.additional_services = [];
+        this.additional_services.forEach((service, index) => {
+            if (service.isSelected) {
+                let newService = frappe.model.get_new_doc("Job Additional Service");
+                newService.parent = this.job_id;
+                newService.parentfield = "additional_services";
+                newService.parenttype = "Job";
+                newService.idx = index + 1;
+                newService.service = service.name;
+                console.log(newService);
+                this.job_detail.additional_services.push(newService);
+            }
+        })
 
+        console.log(this.job_detail);
     }
 
+    renderJobDetail() {
+        $(this.jobDetailElement).empty();
+        $(frappe.render_template('result', {
+            isInstaller: frappe.user.has_role("Field Installer"),
+            installation_type_text: this.selectedInstallationType,
+            result: this.job_detail,
+            site_unit: this.site_unit,
+            site: this.site
+        })).appendTo($(this.jobDetailElement));
+    }
 
 }
