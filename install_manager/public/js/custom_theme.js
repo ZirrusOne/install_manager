2a900a2cd0c488e1674be263b1744d2d9c67a0a6
Joshua Restivo <jrestivo@zirrusone.com>
2022-01-01 17:22:50 +0000

    More purging of non-app-specific stuff

--- install_manager/public/js/custom_theme.js
+++ install_manager/public/js/custom_theme.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
+    $('html').addClass('z1n-im');
 
     let isLevel1Team = frappe.user && frappe.user.has_role // undefined when not login
         && (frappe.user.has_role("Field Lead") || frappe.user.has_role("Field Installer"))
         && !frappe.user.has_role("Field Manager");
     if (isLevel1Team) {
-        $('body').addClass('z1n-field-crew');
+        $('body').addClass('z1n-im-field-crew');
     }
 });
 
